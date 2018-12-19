9807f8304a7b85563f8ab44e681d11b724f14f67
Richard Massey <masseyr44@gmail.com>
2018-12-19 00:28:00 -0700

    added File and Timer classes

--- common.py
+++ common.py
@@ -1,7 +1,28 @@
 import os
 import json
+import time
+import shutil
+import fnmatch
 import numpy as np
+from functools import wraps
 from osgeo import gdal, osr, gdal_array, ogr
+
+
+def _find_dict_index(list_of_dicts,
+                     key,
+                     value):
+    """
+    Method to find where a key value pair occurs in a list of dictionaries
+    :param list_of_dicts: List of dictionaries
+    :param key: Key to find
+    :param value: Value associated with the key
+    :return: index (int)
+    """
+    for i, i_dict in enumerate(list_of_dicts):
+        for k, v in i_dict.items():
+            if k == key:
+                if v == value:
+                    return i
 
 
 class Raster(object):
@@ -473,9 +494,12 @@ class Vector(object):
                     val = float(x)
                 except ValueError:
                     try:
-                        val = str(x)
-                    except:
-                        val = None
+                        val = bool(x)
+                    except ValueError:
+                        try:
+                            val = str(x)
+                        except:
+                            val = None
 
             return Vector.ogr_data_type(val)
 
@@ -667,9 +691,8 @@ class Vector(object):
         :param dest_spatial_ref_str: Destination spatial reference string
         :param dest_spatial_ref_str_type: Destination spatial reference string type
         :param destination_spatial_ref: OSR spatial reference object for destination feature
-        :param epsg: Destination EPSG SRID code (default: 4326)
+        :param epsg: Destination EPSG SRID code
         :return: Reprojected vector object
-        Todo: add write to file method
         """
 
         vector = Vector()
@@ -768,18 +791,275 @@ class Vector(object):
             self.crs_string = vector.crs_string
 
 
-def _find_dict_index(list_of_dicts,
-                     key,
-                     value):
+class File(object):
     """
-    Method to find where a key value pair occurs in a list of dictionaries
-    :param list_of_dicts: List of dictionaries
-    :param key: Key to find
-    :param value: Value associated with the key
-    :return: index (int)
+    Class to handle file and folder operations
     """
-    for i, i_dict in enumerate(list_of_dicts):
-        for k, v in i_dict.items():
-            if k == key:
-                if v == value:
-                    return i
+    def __init__(self,
+                 filename=None):
+        """
+        Initialize the File class
+        :param filename: Name of the file
+        """
+        self.sep = os.path.sep
+
+        if self.filename is not None:
+            self.basename = os.path.basename(filename)
+            self.dirpath = os.path.dirname(filename)
+        else:
+            self.basename = None
+            self.dirpath = None
+
+    def __repr__(self):
+        """
+        Object Representation
+        :return: String
+        """
+        return '<File handler for {}>'.format(self.filename)
+
+    def dir_create(self,
+                   _return=False):
+        """
+        Create dir if it doesnt exist
+        :param: directory to be created
+        """
+        if not os.path.exists(self.dirpath):
+            os.makedirs(self.dirpath)
+        else:
+            pass
+        if _return:
+            return self.dirpath
+
+    def file_exists(self):
+        """
+        Check file existence
+        :return: Bool
+        """
+        if self.filename is None:
+            return
+        else:
+            return os.path.isfile(self.filename)
+
+    def dir_exists(self):
+        """
+        Check folder existence
+        :return: Bool
+        """
+        return os.path.isdir(self.dirpath)
+
+    def dir_delete(self):
+        """
+        Delete a directory and everything in it
+        :return:
+        """
+        shutil.rmtree(self.dirpath,
+                      ignore_errors=True)
+
+    def file_delete(self):
+        """
+        Delete a file
+        """
+        os.remove(self.filename)
+
+    def file_copy(self,
+                  destination_dir=None,
+                  destination_file=None,):
+
+        if destination_dir is not None:
+            shutil.copyfile(self.filename,
+                            destination_dir + self.sep + self.basename)
+        elif destination_file is not None:
+            shutil.copyfile(self.filename,
+                            destination_file)
+
+    def get_dir(self):
+        """
+        Get current dir name
+        :return: string
+        """
+        return os.getcwd()
+
+    def add_to_filename(self,
+                        string,
+                        remove_check=True,
+                        remove_ext=False):
+        """
+        Add a string before file extension
+        :param string: String to be added
+        :param remove_check: Check file for removal (Default: True)
+        :param remove_ext: Should the file extension be removed (Default: False)
+        :return: file name
+        """
+        components = self.basename.split('.')
+
+        if not remove_ext:
+
+            if len(components) >= 2:
+                outfile = self.dirpath + self.sep + '.'.join(components[0:-1]) + \
+                       string + '.' + components[-1]
+            else:
+                outfile = self.basename + self.sep + components[0] + string
+
+        else:
+
+            if len(components) >= 2:
+                outfile = self.dirpath + self.sep + '.'.join(components[0:-1]) + \
+                       string
+            else:
+                outfile = self.basename + self.sep + components[0] + string
+
+        if remove_check:
+            File(outfile).file_remove_check()
+
+        return outfile
+
+    def file_remove_check(self):
+        """
+        Remove a file silently; if not able to remove, change the filename and move on
+        :return filename
+        """
+
+        # if file does not exist, try to create dir
+        if not os.path.isfile(self.filename):
+            self.dir_create()
+
+        # if file exists then try to delete or
+        # get a filename that does not exist at that location
+        counter = 1
+        while os.path.isfile(self.filename):
+            # print('File exists: ' + filename)
+            # print('Deleting file...')
+            try:
+                os.remove(self.filename)
+            except OSError:
+                print('File already exists. Error deleting file!')
+                components = self.basename.split('.')
+                if len(components) < 2:
+                    self.filename = self.dirpath + self.sep + self.basename + '_' + str(counter)
+                else:
+                    self.filename = self.dirpath + self.sep + ''.join(components[0:-1]) + \
+                               '_(' + str(counter) + ').' + components[-1]
+                # print('Unable to delete, using: ' + filename)
+                counter = counter + 1
+        return self.filename
+
+    def find_all(self,
+                 pattern='*'):
+        """
+        Find all the names that match pattern
+        :param pattern: pattern to look for in the folder
+        """
+        result = []
+        # search for a given pattern in a folder path
+        if pattern == '*':
+            search_str = '*'
+        else:
+            search_str = '*' + pattern + '*'
+
+        for root, dirs, files in os.walk(self.dirpath):
+            for name in files:
+                if fnmatch.fnmatch(name, search_str):
+                    result.append(os.path.join(root, name))
+
+        return result  # list
+
+    def find_multiple(self,
+                      pattern_list):
+        """
+        Find all the names that match pattern
+        :param pattern_list: List of patterns to look for in the folder
+        """
+        result = list()
+
+        for i in range(0, len(pattern_list)):
+            temp = self.find_all(pattern_list[i])
+
+            for j in range(0, len(temp)):
+                result.append(temp[j])
+
+        return result
+
+
+class Timer:
+    """
+    Decorator class to measure time a function takes to execute
+    """
+    def __init__(self,
+                 func):
+        self.func = func
+
+    @staticmethod
+    def display_time(seconds,
+                     precision=3):
+        """
+        method to display time in human readable format
+        :param seconds: Number of seconds
+        :param precision: Decimal precision
+        :return: String
+        """
+
+        # define denominations
+        intervals = [('weeks', 604800),
+                     ('days', 86400),
+                     ('hours', 3600),
+                     ('minutes', 60),
+                     ('seconds', 1)]
+
+        # initialize list
+        result = list()
+
+        # coerce to float
+        dtype = type(seconds).__name__
+        if dtype != 'int' or dtype != 'long' or dtype != 'float':
+            try:
+                seconds = float(seconds)
+            except (TypeError, ValueError, NameError):
+                print("Type not coercible to Float")
+
+        # break denominations
+        for name, count in intervals:
+            if name != 'seconds':
+                value = seconds // count
+                if value:
+                    seconds -= value * count
+                    if value == 1:
+                        name = name.rstrip('s')
+                    value = str(int(value))
+                    result.append("{v} {n}".format(v=value,
+                                                   n=name))
+            else:
+                value = "{:.{p}f}".format(seconds,
+                                          p=precision)
+                result.append("{v} {n}".format(v=value,
+                                               n=name))
+
+        # join output
+        return ' '.join(result)
+
+    @classmethod
+    def timing(cls):
+        """
+        Function to compute timing for input function
+        :return: Function and prints time taken
+        """
+
+        def time_it(func):
+
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+
+                t1 = time.time()
+                val = func(*args, **kwargs)
+                t2 = time.time()
+
+                # time to run
+                t = Timer.display_time(t2 - t1)
+
+                print("Time it took to run {}: {}\n".format(func.__name__,
+                                                            t))
+                return val
+
+            return wrapper
+
+        return time_it
+
