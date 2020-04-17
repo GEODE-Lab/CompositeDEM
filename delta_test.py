a47a461812fd5e29fa56b3e824e7803c9c19e1e0
Richard Massey <rm885@nau.edu>
2020-04-16 18:10:30 -0700

    Update delta_test.py

--- delta_test.py
+++ delta_test.py
@@ -1,4 +1,4 @@
-from dem_code.delta import FillVoids, np
+from tilegrid import Layer, np, Edge
 
 
 if __name__ == '__main__':
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     ar = np.array([[8,  7,  9,  3,  3, 4,  5],
                    [9,  6,  4, -1,  8, 9,  9],
                    [2, -1,  5, -1, -1, 4,  9],
-                   [2, -1,  0,  3, -1, -1, 4],
+                   [-1, -1,  -1,  -1, -1, -1, 1],
+                   [-1, -1, -1, -1, -1, -1, -1],
                    [4, -1, -1,  8,  2,  4, 8],
                    [9,  3,  8,  2,  4,  6, 8]])
 
@@ -14,23 +15,33 @@ if __name__ == '__main__':
 
     print('\n')
 
-    arr1 = FillVoids(array=ar, nodata=-1)
+    arr1 = Layer(array=ar, nodata=-1)
 
-    arr1x = np.apply_along_axis(arr1.fill_voids_1d,
-                                1,
-                                arr1.array,
-                                arr1.nodata)
-
-    print(arr1x)
-    print('\n')
-
-    arr1y = np.apply_along_axis(arr1.fill_voids_1d,
-                                0,
-                                arr1.array,
-                                arr1.nodata)
-
-    print(arr1y)
-    print('\n')
+    print(arr1)
 
     arr1f = arr1.fill()
     print(arr1f)
+    print('------------------')
+
+    lw = np.array([
+        Edge.get_nearest_loc_val(ar[0, :], -1),
+        Edge.get_nearest_loc_val(ar[1, :], -1),
+        Edge.get_nearest_loc_val(ar[2, :], -1),
+        Edge.get_nearest_loc_val(ar[3, :], -1),
+        Edge.get_nearest_loc_val(ar[4, :], -1),
+        Edge.get_nearest_loc_val(ar[5, :], -1),
+        Edge.get_nearest_loc_val(ar[6, :], -1),
+
+    ])
+
+    print(lw)
+
+    exit()
+
+
+    a=[[1,2],[2,4],[1,9],[1,5],[2,1],[1,4],[3,2],[3,1],[2,2],[2,3]]
+
+    b = sorted(a, key=lambda x: (x[0], x[1]))
+
+    print(b)
+
