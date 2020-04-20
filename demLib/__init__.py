01db17bf78c65e0116e091024bdcbaa6b81f0fc3
Richard Massey <rm885@nau.edu>
2020-04-20 15:29:05 -0700

    Create __init__.py

---
+++ demLib/__init__.py
@@ -0,0 +1,6 @@
+from demLib.common import File, Timer, group_multi, group_consecutive
+from demLib.spatial import Raster, Vector
+from demLib.tilegrid import Layer, Tile, Edge, TileGrid
+from demLib.exceptions import FieldError, FileNotFound, TileNotFound, \
+    UninitializedError, ObjectNotFound, AxisError
+
